8f880928ca802d50a143d1d71b89061942ab0db7
Mark Doughty <mdoughty@lincoln.ac.uk>
2022-03-15 10:28:49 +0000

    Week 4

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -6,24 +6,24 @@ namespace CMP1903MWorkshopCode
     {
         static void Main(string[] args)
         {
-            //Week 3
-            //Challenge: Get the message from the encrypted text
+            //Week 4
+            //Challenge:  Determine what the value of the accumulator will be when the code is finished running
             //
-            //1. EITHER:    Read in the .txt file into a List of Strings (or anything else?)
-            //   OR:        Read in each line one at a timen and get its maximum character 
-            //2. For each string:
-            //      Count each character in the string
-            //      The maximum is the character
-            //3. Output the unencrypted message.
+            //Read each line of the input.
+            //Determine whether it is a 'acc' or 'jmp' command for each line
+            //Determine the value on each line
+            //Find the value of 'acc' at the end
+            //HINT: We can read each line as an array (or List) of strings; check [0] for 'a' or 'j', then check [4] and [5] for the values
+            //OR: we could split each string at the space into an operation string and a value string
+            //Can we read line by line instead of storing in a collection?
 
-            //Week 3
-            //Task 1: create Message object, call the readTextFromInput() method
-            //Task 2: create Atbash object Step 1: pass message to encode() method Step 2: put in the encode logic
-            //Task 3: put in the decode logic to the decode() method, check if it works
-            //Task 4: read text to encode from a file
 
-          
+            //Week 4
+            //Task 1: Add constructors to the Person() class
+            //Task 2: create a 'screenName' field within Person
+            //Task 3: Create a 'collection' of Person, iterate (i.e. loop) through the collection outputting data for each Person
 
+      
         }
     }
 }
