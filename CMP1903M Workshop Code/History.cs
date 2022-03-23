88c46e9a694106079a82839b8857a6cd875f5fc4
Mark Doughty <doughty.mark@gmail.com>
2022-03-23 17:39:31 +0000

    Week 5

---
+++ CMP1903M Workshop Code/History.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903MWorkshopCode
+{
+    class History
+    {
+        private Stack<Page> history;
+
+        //constructor
+        public History()
+        {
+            history = new Stack<Page>();
+        }
+
+
+
+    }
+}
--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -6,24 +6,27 @@ namespace CMP1903MWorkshopCode
     {
         static void Main(string[] args)
         {
-            //Week 4
-            //Challenge:  Determine what the value of the accumulator will be when the code is finished running
+            //Week 5
+            //Challenge: What are the valid pass phrases?
             //
-            //Read each line of the input.
-            //Determine whether it is a 'acc' or 'jmp' command for each line
-            //Determine the value on each line
-            //Find the value of 'acc' at the end
-            //HINT: We can read each line as an array (or List) of strings; check [0] for 'a' or 'j', then check [4] and [5] for the values
-            //OR: we could split each string at the space into an operation string and a value string
-            //Can we read line by line instead of storing in a collection?
 
 
-            //Week 4
-            //Task 1: Add constructors to the Person() class
-            //Task 2: create a 'screenName' field within Person
-            //Task 3: Create a 'collection' of Person, iterate (i.e. loop) through the collection outputting data for each Person
 
-      
+            //Week 5
+            //Task 1: Add a page to the 'history'
+            //Task 2: Add exceptions
+            //Task 3: Use custom exceptions
+
+            //example of creating a page with a title and URL
+            //Test... remove this when you have seen it working
+            Page p = new Page("Test Page", "http://www.lincoln.ac.uk");
+            Console.WriteLine(p.Address);
+            //.......
+
+            //creating a new, empty history
+            History h1 = new History();
+
+
         }
     }
 }
