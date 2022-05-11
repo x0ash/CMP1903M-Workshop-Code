d35822c61eb292d6bf6940ac139d2f11df6fac50
Mark Doughty <doughty.mark@gmail.com>
2022-05-11 17:06:42 +0100

    Week 10

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -4,29 +4,16 @@ namespace CMP1903MWorkshopCode
 {
     class Program
     {
+        public delegate void delegateWriter(string message);
         static void Main(string[] args)
         {
 
-            //Week 9
-            //Golf Club Management
-            Console.WriteLine("**Welcome to the Golf Club Membership App**");
-            //List of members
-            List<GolfClubMemberBase> list = new List<GolfClubMemberBase>();
-            list.Add(new Member("Fred", new DateTime(1995, 01, 01)));
-            list.Add(new JuniorMember("Freda", new DateTime(2004, 01, 01)));
+            //Week 10
+            //Using delegates
+            //File is created here in the same dir as the exe
+            //Changethe path for somewhere else
+            Writer writer = new Writer("log.txt");
 
-            foreach (GolfClubMemberBase member in list)
-            {
-
-                if (member is Member)
-                {
-                    member.emailAlert("Member: Email message...");
-                }
-                if (member is JuniorMember)
-                {
-                    member.emailAlert("Junior Member: Email message...");
-                }
-            }
 
 
         }
---
+++ CMP1903M Workshop Code/Writer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903MWorkshopCode
+{
+    class Writer
+    {
+        StreamWriter sw;
+
+        //constructor
+        public Writer(string path)
+        {
+            sw = File.CreateText(path);
+        }
+
+        public void write(string text)
+        {
+            sw.WriteLine(text);
+        }
+
+        public void flush()
+        {
+            sw.Flush();
+
+        }
+
+        public void close()
+        {
+            sw.Close();
+        }
+
+    }
+}
