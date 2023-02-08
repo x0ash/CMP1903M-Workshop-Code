a4caab05d0d6d2ae9cf3117c1240265741ab59f4
Ash Duck <Ash@Ashs-MacBook-Air.local>
2023-02-08 10:05:26 +0000

    Update Program.cs

--- CMP1903M Workshop Code/Program.cs
+++ CMP1903M Workshop Code/Program.cs
@@ -7,6 +7,7 @@ namespace CMP1903MWorkshopCode
         public delegate void delegateWriter(string message);
         static void Main(string[] args)
         {
+<<<<<<< HEAD
 
             //Week 10
             //Using delegates
@@ -16,6 +17,28 @@ namespace CMP1903MWorkshopCode
 
 
 
+=======
+            //Problem 1
+            //(-4, -5) and (-1, -1) describe two points. What is the distance between them?
+            int x1 = -4;
+            int y1 = -4;
+
+            int x2 = -1;
+            int y2 = -1;
+
+            int answer1 = 0;
+
+            //Problem 2
+            //What is the denominator of the slope between these points?
+
+
+            //Problem 3
+            //What is the greatest common divisor (GCD) of 1124 and 136?
+
+
+            //Problem 4
+            //What is the remainder, when you divide the square of the sum of the first ten natural numbers by ten?
+>>>>>>> parent of 531c26a (Workshop 2)
         }
     }
 }
